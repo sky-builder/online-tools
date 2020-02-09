9b144ffb29c3f20cefdaf572ed79a07acdcbf997
lsq <breakthroughtgw@gmail.com>
2020-02-09 22:13:53 +0800

    dev

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "1"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd /root && rm -rf online-tools && git clone https://github.com/sky-builder/online-tools.git && cd online-tools && yarn install && yarn run generate && rm -rf /var/www/online-tools && mkdir /var/www/online-tools && cp -r dist/* /var/www/online-tools"
 
 # sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
 # put -r ./build/* /var/www/html
