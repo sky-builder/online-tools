9cd13de03105632b168c813a0b8ab050ff35fa54
lsq <breakthroughtgw@gmail.com>
2020-02-29 18:18:57 +0800

    chore: 设置默认字体

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,8 +11,9 @@
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
+    Roboto, 'Helvetica Neue', 'Microsoft Yahei', Arial, sans-serif;
+  font-size: 100%;
+  line-height: 1.5;
 }
 
 *,
