30781f413db3f5d7fb8c1f8824ddf497f313071b
lsq <breakthroughtgw@gmail.com>
2020-03-10 21:37:25 +0800

    chore: make input required

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,12 +2,12 @@
   <div class="contact">
     <h1>给作者留言</h1>
     <form action="" @submit.prevent="doSend">
-      <label class="block mt-2" for="js-name">名字</label>
-      <input name="name" type="text" id="js-name" class="w-64">
-      <label class="block mt-2" for="js-email">邮箱</label>
-      <input name="email" type="email" id="js-email" class="w-64">
-      <label class="mt-2 block" for="js-content">内容</label>
-      <textarea name="content" id="js-content" cols="30" rows="10" class="w-full"></textarea>
+      <label class="block mt-2" for="js-name" >名字</label>
+      <input name="name" type="text" id="js-name" class="w-64" required>
+      <label class="block mt-2" for="js-email" >邮箱</label>
+      <input name="email" type="email" id="js-email" class="w-64" required>
+      <label class="mt-2 block" for="js-content" > 内容</label>
+      <textarea name="content" id="js-content" cols="30" rows="10" class="w-full" required></textarea>
       <button type="submit">发送</button>
     </form>
   </div>
