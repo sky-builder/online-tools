295f8f12d093634b643be2277b8e522771afcb60
lsq <breakthroughtgw@gmail.com>
2020-03-10 21:11:19 +0800

    feat: save message

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="contact">
     <h1>给作者留言</h1>
-    <label class="block mt-2" for="js-name">名字</label>
-    <input type="text" id="js-name" class="w-64">
-    <label class="block mt-2" for="js-email">邮箱</label>
-    <input type="email" id="js-email" class="w-64">
-    <label class="mt-2 block" for="js-content">内容</label>
-    <textarea name="" id="js-content" cols="30" rows="10" class="w-full"></textarea>
-    <button>发送</button>
+    <form action="" @submit.prevent="doSend">
+      <label class="block mt-2" for="js-name">名字</label>
+      <input name="name" type="text" id="js-name" class="w-64">
+      <label class="block mt-2" for="js-email">邮箱</label>
+      <input name="email" type="email" id="js-email" class="w-64">
+      <label class="mt-2 block" for="js-content">内容</label>
+      <textarea name="content" id="js-content" cols="30" rows="10" class="w-full"></textarea>
+      <button type="submit">发送</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  methods: {
+    doSend(e) {
+      let form = e.target;
+      let formData = new FormData(form);
+      let data = {};
+      formData.forEach((value, key) => {
+        data[key] = value
+      })
+      axios.post('http://api.magisk.tech/message', data)
+      .then(resp => {
+        form.reset();
+      })
+      .catch(e => {
+        console.log('err', e);
+      })
+    }
+  }
 }
 </script>
 
