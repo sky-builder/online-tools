4cf40bff688136ab181df5ea91d64dc83b4d7165
lsq <breakthroughtgw@gmail.com>
2020-04-04 21:46:18 +0800

    chore: http 2 https

--- pages/life/bing-daily-photo.vue
+++ pages/life/bing-daily-photo.vue
@@ -44,7 +44,7 @@ export default {
       // For local dev
       // const imageUrl = 'http://www.bing.com/th?id=OHR.NewYearFuji_ROW7249160512_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp';
       const baseUrl = "http://www.bing.com";
-      const result = await axios.get("http://api.magisk.tech/bing-daily-photo");
+      const result = await axios.get("https://api.magisk.tech/bing-daily-photo");
       const data = result.data;
       const imageUrl = baseUrl + data.images[0].url;
       let img = new Image();
